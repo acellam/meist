e3a6bb3d5295cf553a1a7f55f17637125ff237d9
Acellam Guy <abiccel@yahoo.com>
2017-12-30 15:52:56 +0300

    Manage issues using state

--- src/App.jsx
+++ src/App.jsx
@@ -70,17 +70,42 @@ const issues = [
     }, ];
 
 class IssueList extends React.Component {
+
+    constructor() {
+        super();
+
+        this.state = { issues: issues };
+
+        setTimeout( this.createTestIssue.bind( this ), 2000 );
+    }
+
     render() {
         return (
             <div>
                 <h1>Issue Tracker</h1>
                 <IssueFilter/>
                 <hr/>
-                <IssueTable issues={issues}/>
+                <IssueTable issues={this.state.issues}/>
                 <hr/>
                 <IssueAdd/>
             </div>
         );
+    }
+
+    createTestIssue() {
+        this.createIssue( {
+            status: "New", owner: "Pieta", created: new Date(),
+            title: "Completion date should be optional",
+        } );
+    }
+
+    createIssue(newIssue) {
+        const newIssues = this.state.issues.slice();
+
+        newIssue.id = this.state.issues.length + 1;
+        newIssues.push( newIssue );
+
+        this.setState( { issues: newIssues } );
     }
 }
 
