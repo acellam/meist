89b5ddea25d31ae5e26ba598ac28daee65816466
Acellam Guy <abiccel@yahoo.com>
2017-12-30 15:13:12 +0300

    Add row component

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,33 @@ class IssueFilter extends React.Component {
 
 class IssueTable extends React.Component {
     render() {
+        const borderedStyle = { border: "1px solid silver", padding: 6 };
         return (
-            <div>This is a placeholder for a table of Issues.</div>
-        )
+            <table style={{ borderCollapse: "collapse" }}>
+                <thead>
+                <tr>
+                    <th style={borderedStyle}>Id</th>
+                    <th style={borderedStyle}>Title</th>
+                </tr>
+                </thead>
+                <tbody>
+                <IssueRow issue_id={1}
+                          issue_title="Error in console when clicking Add"/>
+                <IssueRow issue_id={2}
+                          issue_title="Missing bottom border on panel"/>
+                </tbody>
+            </table> )
+    }
+}
+
+class IssueRow extends React.Component {
+    render() {
+        const borderedStyle = { border: "1px solid silver", padding: 4 };
+        return (
+            <tr>
+                <td style={borderedStyle}>{this.props.issue_id}</td>
+                <td style={borderedStyle}>{this.props.issue_title}</td>
+            </tr> )
     }
 }
 
