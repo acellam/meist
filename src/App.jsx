b9360e20d84a1ef60831d532af385e4b57cdf083
Acellam Guy <abiccel@yahoo.com>
2018-01-01 16:15:52 +0300

    Add Hot Module Reload

--- src/App.jsx
+++ src/App.jsx
@@ -5,5 +5,8 @@ import IssueList from "./IssueList.jsx";
 
 const contentNode = document.getElementById( "contents" );
 
-
 ReactDOM.render( <IssueList/>, contentNode ); // Render the component inside
+
+if (module.hot) {
+    module.hot.accept();
+}
--- webpack.config.js
+++ webpack.config.js
@@ -23,4 +23,12 @@ module.exports = {
                 },
             } ],
     },
+    devServer: {
+        port: 8000,
+        contentBase: "static",
+        proxy: {
+            "/api/*": {
+                target: "http://localhost:3000",
+            } },
+    },
 };
