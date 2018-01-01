403ae4e0a3df7e1c7f35d4cdda10abcfaec0af47
Acellam Guy <abiccel@yahoo.com>
2018-01-01 15:51:35 +0300

    Bundle react with webpack

--- src/IssueList.jsx
+++ src/IssueList.jsx
@@ -1,36 +1,39 @@
+import React from "react";
+import "whatwg-fetch";
+
 import IssueAdd from "./IssueAdd.jsx";
 import IssueFilter from "./IssueFilter.jsx";
 
 const IssueRow = props => (
     <tr>
         <td>{props.issue._id}</td>
-        <td>{props.issue.status}</td>
-        <td>{props.issue.owner}</td>
+    <td>{props.issue.status}</td>
+    <td>{props.issue.owner}</td>
         <td>{props.issue.created.toDateString()}</td>
         <td>{props.issue.effort}</td>
         <td>{props.issue.completionDate ? props.issue.completionDate.toDateString() : ""}</td>
-        <td>{props.issue.title}</td>
-    </tr>
+    <td>{props.issue.title}</td>
+  </tr>
 );
 
-function IssueTable(props) {
-    const issueRows = props.issues.map( issue => <IssueRow key={issue._id} issue={issue}/> );
+function IssueTable( props ) {
+    const issueRows = props.issues.map( issue => <IssueRow key={issue._id} issue={issue} /> );
 
     return (
         <table className="bordered-table">
-            <thead>
-            <tr>
-                <th>Id</th>
-                <th>Status</th>
-                <th>Owner</th>
-                <th>Created</th>
-                <th>Effort</th>
-                <th>Completion Date</th>
-                <th>Title</th>
-            </tr>
+        <thead>
+              <tr>
+                    <th>Id</th>
+                    <th>Status</th>
+                    <th>Owner</th>
+                    <th>Created</th>
+                    <th>Effort</th>
+                  <th>Completion Date</th>
+                    <th>Title</th>
+                </tr>
             </thead>
-            <tbody>{issueRows}</tbody>
-        </table>
+        <tbody>{issueRows}</tbody>
+      </table>
     );
 }
 
@@ -49,12 +52,12 @@ export default class IssueList extends React.Component {
         return (
             <div>
                 <h1>Issue Tracker</h1>
-                <IssueFilter/>
-                <hr/>
-                <IssueTable issues={this.state.issues}/>
-                <hr/>
-                <IssueAdd createIssue={this.createIssue}/>
-            </div>
+                <IssueFilter />
+                <hr />
+                <IssueTable issues={this.state.issues} />
+            <hr />
+                <IssueAdd createIssue={this.createIssue} />
+          </div>
         );
     }
 
@@ -65,49 +68,46 @@ export default class IssueList extends React.Component {
     // PRIVATE METHODS
 
     loadData() {
-        fetch('/api/issues').then(response => {
-            if (response.ok) {
-                response.json().then(data => {
-                    console.log("Total count of records:", data._metadata.total_count);
-                    data.records.forEach(issue => {
-                        issue.created = new Date(issue.created);
-                        if (issue.completionDate)
-                            issue.completionDate = new Date(issue.completionDate);
-                    });
-                    this.setState({ issues: data.records });
-                });
+        fetch( "/api/issues" ).then( ( response ) => {
+            if ( response.ok ) {
+                response.json().then( ( data ) => {
+                    console.log( "Total count of records:", data._metadata.total_count );
+                    data.records.forEach( ( issue ) => {
+                        issue.created = new Date( issue.created );
+                        if ( issue.completionDate ) { issue.completionDate = new Date( issue.completionDate ); }
+                    } );
+                    this.setState( { issues: data.records } );
+                } );
             } else {
-                response.json().then(error => {
-                    alert("Failed to fetch issues:" + error.message)
-                });
+                response.json().then( ( error ) => {
+                    alert( `Failed to fetch issues:${ error.message }` );
+                } );
             }
-        }).catch(err => {
-            alert("Error in fetching data from server:", err);
-        });
+        } ).catch( ( err ) => {
+            alert( "Error in fetching data from server:", err );
+        } );
     }
 
-
-    createIssue(newIssue) {
+    createIssue( newIssue ) {
         fetch( "/api/issues", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify( newIssue ),
-        } ).then( response => {
-            if (response.ok) {
-                response.json().then( updatedIssue => {
+        } ).then( ( response ) => {
+            if ( response.ok ) {
+                response.json().then( ( updatedIssue ) => {
                     updatedIssue.created = new Date( updatedIssue.created );
-                    if (updatedIssue.completionDate)
-                        updatedIssue.completionDate = new Date( updatedIssue.completionDate );
+                    if ( updatedIssue.completionDate ) { updatedIssue.completionDate = new Date( updatedIssue.completionDate ); }
                     const newIssues = this.state.issues.concat( updatedIssue );
                     this.setState( { issues: newIssues } );
                 } );
             } else {
-                response.json().then( error => {
-                    alert( "Failed to add issue: " + error.message )
+                response.json().then( ( error ) => {
+                    alert( `Failed to add issue: ${ error.message }` );
                 } );
             }
-        } ).catch( err => {
-            alert( "Error in sending data to server: " + err.message );
+        } ).catch( ( err ) => {
+            alert( `Error in sending data to server: ${ err.message }` );
         } );
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
+const webpack = require( "webpack" );
 const path = require( "path" );
 
 module.exports = {
-    entry: "./src/App.jsx",
+    entry: {
+        app: "./src/App.jsx",
+        vendor: [ "react", "react-dom", "whatwg-fetch" ],
+    },
     output: {
         path: path.resolve( __dirname, "static" ),
         filename: "app.bundle.js",
     },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin( { name: "vendor", filename: "vendor.bundle.js" } ),
+    ],
     module: {
         loaders: [
             {
